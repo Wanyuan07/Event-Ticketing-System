08d91a9cfc195fee338400429f5057e5c431e694
Priyanshi <priyanshi.sah07@gmail.com>
2026-07-09 13:20:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ for (let i = 0; i < button.length; i++) {
     })
 }  
 
+let totalSeats = document.getElementsByClassName("totalSeats")
+let seats = JSON.parse(localStorage.getItem("seat-number")) || {};
+for (let i = 0; i<eventName.length; i++) {
+    if(!seats[eventName[i].innerText]) {seats[eventName[i].innerText] = 0};
+}
+localStorage.setItem("seat-number", JSON.stringify(seats));
+console.log(seats)
+let seatsRemaining = document.getElementsByClassName("seats-remaining");
+for (let i = 0; i<seatsRemaining.length; i++) {
+    seatsRemaining[i].innerHTML = `Seats filled: ${seats[eventName[i].innerText]}
+                                   (${totalSeats[i].innerHTML.split(" ")[3] - seats[eventName[i].innerText]} left)`;
+}
